f6468f06f1a7f65492276f1b5daebce5bf18226e
Nikita <6025342+nikita-kun@users.noreply.github.com>
2024-11-20 22:30:42 +0100

    [Update] Removed unused files

--- .eustia.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  test: {
-    library: ['node_modules/eustia-module'],
-    files: ['test/*.js', 'test/*.html'],
-    exclude: ['js'],
-    namespace: 'util',
-    output: 'test/util.js',
-  },
-}
